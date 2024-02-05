d0e59eb1597c2cf83029636fe86ddf4546602078
sonoou <sonu26681094@gmail.com>
2024-02-05 23:13:17 +0530

    source code

--- Class_26_inheritance_1.feb.2024/super_keyword_and_init_block/Init.java
+++ Class_26_inheritance_1.feb.2024/super_keyword_and_init_block/Init.java
@@ -11,16 +11,36 @@ class A{
 
 class B extends A{
 	{
-		System.out.println("init block of class A");
+		System.out.println("init block of class B");
 	}
 	B(int x){
 		//super();
 		// init block code will be pasted here after compilation
-		System.out.println("B");
+		System.out.println(x);
 	}
 	B(){
 		//super();
 		//init block code will be pasted here after compilation
+		System.out.println("B");
+	}
+}
+
+class C extends B{
+	{
 		System.out.println("init block of class C");
 	}
-	
+	C(){
+		super(10);
+		//init block will be pasted here after compilation
+		System.out.println("C");
+	}
+	C(int x){
+		//super();
+		//init block code will be pasted here after compilation
+		System.out.println(x);
+	}
+	public static void main(String... s){
+		new C();
+		//new C(12);
+	}
+}
